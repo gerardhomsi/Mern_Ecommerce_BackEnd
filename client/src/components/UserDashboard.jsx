6de19b5212a7a3134c946613bd842ee6cc003125
Gerard Homsi <gerard.homsi@gmail.com>
2023-06-16 12:38:39 -0500

    test

--- client/src/components/UserDashboard.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UserDashboard = () => {
-  return <div>USER DASHBOARD</div>;
-};
-
-export default UserDashboard;
--- server.js
+++ server.js
@@ -27,3 +27,4 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+// kol khara
